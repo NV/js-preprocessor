422c5615c4eaabbc98f3df6ef18d51634f33f71c
Nikita Vasilyev <me@elv1s.ru>
2010-01-03 04:31:42 +0300

    Test path fixed. Rake now should work.

--- test/jspp_test.rb
+++ test/jspp_test.rb
@@ -2,18 +2,22 @@ require "test/unit"
 
 class JSPreprocessorTest < Test::Unit::TestCase
 
-  def jspp(arg='')
-    `../bin/jspp ../example/#{arg}`
+  DIR = File.dirname __FILE__
+  EXAMPLE = File.join DIR, '..', 'example', ''
+  JSPP = File.join DIR, '..', 'bin', 'jspp'
+
+  def jspp(name='')
+    `#{JSPP} #{EXAMPLE}#{name}`
   end
 
   def test_examples
-    assert_equal File.read('../example/style-child.css'), jspp('style-child.css')
+    assert_equal File.read("#{EXAMPLE}style-child.css"), jspp('style-child.css')
     assert_equal <<-multiline, jspp('style.css')
 html, body {margin:0; padding:0}
 .i-am-child {}
 .my-style {background: #fffacc}
 multiline
-    assert_equal File.read('../example/script.js'), jspp('script.js')
+    assert_equal File.read("#{EXAMPLE}script.js"), jspp('script.js')
     assert_equal <<-multiline, jspp('main.js')
 var my_script;
 
