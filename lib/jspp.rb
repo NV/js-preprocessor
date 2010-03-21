539f8d7707fbf961fc6e089c6d07fd97f4982156
Nikita Vasilyev <me@elv1s.ru>
2010-03-21 20:45:24 +0300

    Oh my! Undo unfinished refactoring.

--- lib/jspp.rb
+++ lib/jspp.rb
@@ -1,52 +1,7 @@
-require 'open-uri'
-
 class JSPP
 
   INCLUDE = %r{/\*>\s*(.+?)\s*\*/}
   INCLUDE_INSIDE_STRING = %r{"#{INCLUDE.source}"}
-
-  def initialize path
-    @path = path
-  end
-  
-  attr_accessor :path, :file, :children
-
-  def read
-    if uri? @path
-      open(@path).read
-    else
-      File.read @path
-    end
-  end
-
-  def tree
-    @file = read
-    @children = @file.scan(INCLUDE).inject([]) {|all, p|
-      p = from_here_to(p) unless uri?(p.first)
-      all << self.class.new(p).tree
-    }
-    self
-  end
-
-  def build
-    
-  end
-
-  def to_s
-    @path
-  end
-
-  def self.uri? p
-    p.index'http://' or p.index'https://'
-  end
-
-  def from_here_to suffix
-    File.join dirname, suffix
-  end
-
-  def dirname
-    File.dirname @path
-  end
 
   def parse text='', base_dir='.'
     text.gsub!(INCLUDE_INSIDE_STRING) {
--- spec/helper.rb
+++ spec/helper.rb
@@ -7,5 +7,5 @@ PETS = "Cat, dog, turtle, hamster, parrot\n"
 PETS_URL = 'http://elv1s.ru/x/pets.txt'
 FakeWeb.register_uri :get, PETS_URL, :body => PETS
 EXAMPLE = DIR + '/../example'
-CHILD_CSS = File.read(EXAMPLE + '/css/style-child.css')
+CHILD_CSS = File.read(EXAMPLE + '/css/child.css')
 FakeWeb.register_uri :get, 'http://js-preprocessor.com/example/css/child.css', :body => CHILD_CSS
